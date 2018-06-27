12dc9593c9b9a7d45a76b70e379691a19a5d556c
Azza-Ahmed <azza.h.mostafa@gmail.com>
2018-06-26 20:33:52 -0700

    separated the model

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 var Cat = function(){
-
-}
-
-var ViewModel = function(){
   this.clickCount = ko.observable(0);
   this.name = ko.observable('kitty');
   this.url = ko.observable('img/22252709_010df3379e_z.jpg');
@@ -28,9 +24,12 @@ var ViewModel = function(){
   }, this);
 
   this.nickNames = ko.observableArray(['Cutie Cat', 'Tommy', 'Bingo', 'Tiger']);
+}
 
+var ViewModel = function(){
+  this.currentCat = ko.observable(new Cat());
   this.incrementCounter = function(){
-    this.clickCount(this.clickCount() + 1);
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
   }
 
 }
