15d84a5c6000b9894fd288efc15d89cc5d1687db
Azza-Ahmed <azza.h.mostafa@gmail.com>
2018-06-26 20:21:18 -0700

    changed title to be a computed observable

--- js/app.js
+++ js/app.js
@@ -1,22 +1,36 @@
+var Cat = function(){
+
+}
+
 var ViewModel = function(){
   this.clickCount = ko.observable(0);
   this.name = ko.observable('kitty');
   this.url = ko.observable('img/22252709_010df3379e_z.jpg');
-  this.title = ko.observable('newborn');
+  this.title = ko.computed(function(){
+    var title;
+    var clicks = this.clickCount();
+    if (clicks > 40){
+      title = 'Adult';
+    }
+    else if (clicks > 30){
+      title = 'Teen';
+    }
+    else if (clicks > 20){
+      title = 'Kid';
+    }
+    else if (clicks > 10){
+      title = 'Infant';
+    }
+    else{
+      title = 'Newborn';
+    }
+    return title;
+  }, this);
 
   this.nickNames = ko.observableArray(['Cutie Cat', 'Tommy', 'Bingo', 'Tiger']);
 
   this.incrementCounter = function(){
     this.clickCount(this.clickCount() + 1);
-    if (this.clickCount() > 30){
-      this.title('teen');
-    }
-    else if (this.clickCount() > 20){
-      this.title('kid');
-    }
-    else if (this.clickCount() > 10){
-      this.title('infant');
-    }
   }
 
 }
