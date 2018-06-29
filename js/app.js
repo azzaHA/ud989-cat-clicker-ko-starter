ddb0a27ca4cc3d68b990cc2e2b1166d453e02ea5
Azza-Ahmed <azza.h.mostafa@gmail.com>
2018-06-28 21:33:16 -0700

    feat: Add list of cats

--- js/app.js
+++ js/app.js
@@ -1,7 +1,46 @@
-var Cat = function(){
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('kitty');
-  this.url = ko.observable('img/22252709_010df3379e_z.jpg');
+var initialCats = [
+  {
+    clickCount: 0,
+    name: 'Kitty',
+    url: 'img/22252709_010df3379e_z.jpg',
+    nickNames: ['Cutie Cat', 'Tommy', 'Bingo', 'Tiger']
+
+  },
+  {
+    clickCount: 0,
+    name: 'Tiger',
+    url: 'img/434164568_fea0ad4013_z.jpg',
+    nickNames: ['Tigger']
+
+  },
+  {
+    clickCount: 0,
+    name: 'Touta',
+    url: 'img/1413379559_412a540d29_z.jpg',
+    nickNames: ['Bingo']
+
+  },
+  {
+    clickCount: 0,
+    name: 'Bingo',
+    url: 'img/4154543904_6e2428c421_z.jpg',
+    nickNames: ['Tommy']
+
+  },
+  {
+    clickCount: 0,
+    name: 'Frank',
+    url: 'img/9648464288_2516b35537_z.jpg',
+    nickNames: ['Sleepy Cat']
+
+  }
+];
+
+var Cat = function(data){
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.url = ko.observable(data.url);
+  this.nickNames = ko.observableArray(data.nickNames);
   this.title = ko.computed(function(){
     var title;
     var clicks = this.clickCount();
@@ -23,15 +62,25 @@ var Cat = function(){
     return title;
   }, this);
 
-  this.nickNames = ko.observableArray(['Cutie Cat', 'Tommy', 'Bingo', 'Tiger']);
+
 }
 
 var ViewModel = function(){
   var self = this;
-  this.currentCat = ko.observable(new Cat());
-  this.incrementCounter = function(){
+  this.catList = ko.observableArray([]);
+  initialCats.forEach(function(cat){
+    self.catList.push(new Cat(cat));
+  });
 
+  this.currentCat = ko.observable(this.catList()[0]);
+
+  this.incrementCounter = function(){
     self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  }
+
+  this.setCurrentCat = function(cat){
+    self.currentCat(cat);
+    console.log(cat.name);
   }
 
 }
